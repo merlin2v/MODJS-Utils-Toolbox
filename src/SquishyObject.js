a6507742f9f0f6e8ff72a360756102fd4efadac3
merlin2v <nathan2visawsome@gmail.com>
2018-04-12 15:06:53 -0700

    Update SquishyObject.js

--- src/SquishyObject.js
+++ src/SquishyObject.js
@@ -1,3 +1,17 @@
+/* 
+ * This is a collection of tools made by Merlin2v
+ */
+
+/**
+ * @example unknownFunction(new PokeyObject())
+ *
+ * @author Merlin2v
+ * @class for testing what a object wants
+ * @throws an error to notify that the function may want something
+ **/
+  /** @constructor
+  * @param {*} [base={}] - the objects values 
+  */
 function SquishyObject(base){
   var handler = {
     get:function(target, property) {
